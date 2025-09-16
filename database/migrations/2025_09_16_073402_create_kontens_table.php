060dbf238aed7a6a85f8cfac6def8236be1c485e
Firyal-dev <firyal.dev@gmail.com>
2025-09-16 15:13:15 +0700

    desain awal tabel dan model

---
+++ database/migrations/2025_09_16_073402_create_kontens_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kontens', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('file_path');
+            $table->date('tgl_upload');
+            $table->enum('konten', ['galeri', 'album']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kontens');
+    }
+};
